f0b711b5622e52704e9a4450d7051dd053350692
kevinrs350z <kevin.rojas.sierra@alumnos.uta.cl>
2026-01-27 16:11:44 -0300

    s

--- Frontend/src/app/components/auth/auth.service.ts
+++ Frontend/src/app/components/auth/auth.service.ts
@@ -22,7 +22,7 @@ interface LoginResponse {
 export class AuthService {
   //private apiUrl = 'https://cofferlike-nonaseptic-stephen.ngrok-free.dev/api'; 
   //private apiUrl = 'http://localhost:8000/api';//}
-  private readonly apiUrl = `${environment.apiBaseUrl}/api/auth`;  
+  private readonly apiUrl = `${environment.apiBaseUrl}/api`;  
 
   constructor(private http: HttpClient) { }
 
