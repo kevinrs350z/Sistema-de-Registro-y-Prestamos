88e60f3f0cb1b82b2aa7b09f62c53f83f4e05245
juan meneses <juan.meneses75m@gmail.com>
2026-02-03 13:03:30 -0300

    merch

--- Backend/database/seeders/RolesTableSeeder.php
+++ Backend/database/seeders/RolesTableSeeder.php
@@ -10,6 +10,8 @@ class RolesTableSeeder extends Seeder
     public function run(): void
     {
         $roles = [
+            ['Nombre' => 'ADMIN', 'Descripcion' => 'Administrador del sistema', 'created_at' => now(), 'updated_at' => now()],
+            ['Nombre' => 'ALUMNO', 'Descripcion' => 'Usuario del sistema', 'created_at' => now(), 'updated_at' => now()],
             ['Nombre' => 'SUPER_USUARIO', 'Descripcion' => 'Gestor avanzado de préstamos y equipos, sin permisos administrativos', 'created_at' => now(), 'updated_at' => now()],
         ];
         foreach ($roles as $rol) {
